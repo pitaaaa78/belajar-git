5153644eab4dfb92ad4dde0ee2d866aa5c3fd392
Ardila Novita Sari <crwiszvita@gmail.com>
2024-03-19 11:07:31 +0700

    belajar git

---
+++ perulangan.php
@@ -0,0 +1,20 @@
+<?php
+$i = 1;
+while ($i <= 10) {
+  echo $i;
+  echo "<br>";
+  $i = $i + 1;
+}
+
+<br />
+<br />
+
+<?php
+$i=100;
+while ($i >= 0)
+{
+   echo "$i";
+   echo "<br />";
+   $i-=8;
+}
+?>
